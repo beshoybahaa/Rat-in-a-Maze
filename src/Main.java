d6ca99f791f24b19f739ac28e9a5d8e0d8cf68e4
Bassem S.Mosaad <bassemsaeed620@gmail.com>
2023-12-11 06:56:47 +0200

    .

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-import java.util.LinkedList;
-
 public class Main {
     // Global Variables
     public static int numberOfVisited = 0;
@@ -48,7 +46,7 @@ public class Main {
     public static void printVisited() {
         for (int i = 0; i < 64; i++) {
             for (int j = 0; j < 3; j++) {
-                System.out.printf(String.valueOf(visited[i][j]) + " ");
+                System.out.printf(visited[i][j] + " ");
             }
             System.out.println();
         }
